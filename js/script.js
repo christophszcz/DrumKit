ce76e5aecc140a308c47c9e51fd161530a280123
christophszcz <christoph.szczechowicz@gmail.com>
2016-12-26 20:10:05 -0500

    Intertwined the sound with the key this is pressed.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 window.addEventListener('keydown', function(e){
-	console.log(e);
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	console.log(audio);
+	audio.currentTime = 0; //this will rewind to the start.
+	if(!audio) return;
+	audio.play();
 });
